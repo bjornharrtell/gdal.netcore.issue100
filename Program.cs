6067581e7e0546c509b45d699f0c6123a4166fa1
Björn Harrtell <bjorn@wololo.org>
2023-05-06 10:42:43 +0200

    Minimal repro

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MaxRev.Gdal.Core;
+
+Console.WriteLine("Hello, World!");
+
+GdalBase.ConfigureAll();
+
+Console.WriteLine("GdalBase.ConfigureAll has run");
